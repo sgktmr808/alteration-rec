f987bf8aac0664755094063319b8f0ae322e46d0
sgktmr808 <sgktmr@gmail.com>
2026-04-05 16:36:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE = 'alterec-v16n';
+const CACHE = 'alterec-v16o';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
