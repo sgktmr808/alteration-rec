41dd6d25423617626801ccb5baf43eba6cf97a75
sgktmr808 <sgktmr@gmail.com>
2026-04-06 04:36:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-const CACHE = 'alterec-v16x';
-const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+// Service Worker を無効化 - キャッシュを一切使わない
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  // 全キャッシュを削除
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
+// キャッシュを使わず常にネットワークから取得
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  if(e.request.mode === 'navigate'){
+    e.respondWith(fetch(e.request.url + '?v=' + Date.now()));
+  }
 });
