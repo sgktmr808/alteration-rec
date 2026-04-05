0a2de1cf79b065636f8e1a26f7a06b0f8f1ef54b
sgktmr808 <sgktmr@gmail.com>
2026-04-05 16:15:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,16 @@
-const CACHE = 'alt3-v5';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'alterec-v16n';
+const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c => c.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  // index.htmlは常にネットワークから取得（キャッシュより優先）
-  if (e.request.url.includes('index.html') || e.request.url.endsWith('/')) {
-    e.respondWith(
-      fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match(e.request))
-    );
-    return;
-  }
-  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
