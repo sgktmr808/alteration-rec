8eb287c6b1952cfcebc4b619adcad06c33968724
sgktmr808 <sgktmr@gmail.com>
2026-04-06 05:43:28 +0900

    update 2026-04-06 05:43

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
-// Service Worker を無効化 - キャッシュを一切使わない
+const CACHE = 'alterec-v16y';
+const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  // 全キャッシュを削除
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
-// キャッシュを使わず常にネットワークから取得
 self.addEventListener('fetch', e => {
-  if(e.request.mode === 'navigate'){
-    e.respondWith(fetch(e.request.url + '?v=' + Date.now()));
-  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
